a65a4c4fbb28ca6c8fbbc966793c4bf2bedd649c
grrctlpd <gerrictl@gmail.com>
2026-03-05 23:49:41 +0800

    Added CRUD functionality for colleges, programs, orgs, students, and members

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,10 +16,43 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+from studentorg.views import (HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, 
+                            OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, 
+                            StudentListView, StudentCreateView, StudentUpdateView, StudentDeleteView,
+                            CollegeListView, CollegeCreateView, CollegeUpdateView, CollegeDeleteView,
+                            ProgramListView, ProgramCreateView, ProgramUpdateView, ProgramDeleteView,)
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
-]
+    
+    #Organization
+    path('organization_list', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+    
+    #OrgMember
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<pk>', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+    
+    #Student
+    path('student/', StudentListView.as_view(), name='student-list'),
+    path('student/add/', StudentCreateView.as_view(), name='student-add'),
+    path('student/<pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('student/<pk>/delete/', StudentDeleteView.as_view(), name='student-delete'),
+    
+    #College
+    path('college/', CollegeListView.as_view(), name='college-list'),
+    path('college/add/', CollegeCreateView.as_view(), name='college-add'),
+    path('college/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
+    path('college/<pk>/delete/', CollegeDeleteView.as_view(), name='college-delete'),
+
+    #Program
+    path('program/', ProgramListView.as_view(), name='program-list'),
+    path('program/add/', ProgramCreateView.as_view(), name='program-add'),
+    path('program/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
+    path('program/<pk>/delete/', ProgramDeleteView.as_view(), name='program-delete'),]
